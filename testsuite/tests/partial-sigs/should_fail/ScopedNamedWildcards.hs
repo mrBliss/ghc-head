1fc444803a83cef66f0aa5fe38dad1f124e3a967
Dominique Devriese <dominique.devriese@cs.kuleuven.be>
2014-02-27 14:16:58 +0100

    add a comment to test

--- testsuite/tests/partial-sigs/should_fail/ScopedNamedWildcards.hs
+++ testsuite/tests/partial-sigs/should_fail/ScopedNamedWildcards.hs
@@ -2,6 +2,8 @@
 
 module ScopedNamedWildcards where
 
+-- if named wildcards are properly scoped, this should lead to
+-- a constraint _a ~ (_a,_)
 test2 :: _a -> _a
 test2 x = let y :: _a
               y = (x,undefined)
