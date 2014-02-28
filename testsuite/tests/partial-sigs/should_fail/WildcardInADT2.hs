a63c405e3d67494a09e1d3c31ada602604f31cbd
Thomas Winant <thomas.winant@cs.kuleuven.be>
2014-02-28 13:35:58 +0100

    No wildcards in (G)ADTs, newtypes & type synonyms

---
+++ testsuite/tests/partial-sigs/should_fail/WildcardInADT2.hs
@@ -0,0 +1,6 @@
+{-# LANGUAGE PartialTypeSignatures #-}
+module WildcardInADT2 where
+
+data Foo a = Foo { get :: Either _ a }
+
+
---
+++ testsuite/tests/partial-sigs/should_fail/WildcardInADT3.hs
@@ -0,0 +1,6 @@
+{-# LANGUAGE PartialTypeSignatures, RankNTypes #-}
+module WildcardInADT3 where
+
+data Foo a = Foo { get :: _ => a }
+
+
