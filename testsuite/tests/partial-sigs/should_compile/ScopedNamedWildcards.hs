80e7c8fdfb7135d7722ff6a512ecb762c2753e26
Dominique Devriese <dominique.devriese@cs.kuleuven.be>
2014-02-28 14:06:06 +0100

    extra test for scoped named wildcards

---
+++ testsuite/tests/partial-sigs/should_compile/ScopedNamedWildcards.hs
@@ -0,0 +1,11 @@
+{-# LANGUAGE PartialTypeSignatures #-}
+
+module ScopedNamedWildcards where
+
+test3 :: _
+test3 x = const (let x :: _b
+                     x = True in False) $
+          const (let x :: _b
+                     x = 'a' in True) $
+          not x
+
