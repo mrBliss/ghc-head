mrBliss/ghc

Desugar named wildcards to meta vars

compiler/typecheck/TcRnMonad.lhs
@@ -91,6 +91,7 @@ initTc hsc_env hsc_src keep_rn_syntax mod do_this
                            Nothing             -> newIORef emptyNameEnv } ;
 
         dependent_files_var <- newIORef [] ;
+        named_wc_map        <- newIORef emptyNamedWildcardMap ;
         let {
              maybe_rn_syntax :: forall a. a -> Maybe a ;
              maybe_rn_syntax empty_val
@@ -140,18 +141,19 @@ initTc hsc_env hsc_src keep_rn_syntax mod do_this
                 tcg_dependent_files = dependent_files_var
              } ;
              lcl_env = TcLclEnv {
-                tcl_errs       = errs_var,
-                tcl_loc        = mkGeneralSrcSpan (fsLit "Top level"),
-                tcl_ctxt       = [],
-                tcl_rdr        = emptyLocalRdrEnv,
-                tcl_th_ctxt    = topStage,
-                tcl_arrow_ctxt = NoArrowCtxt,
-                tcl_env        = emptyNameEnv,
-                tcl_tidy       = emptyTidyEnv,
-                tcl_tyvars     = tvs_var,
-                tcl_lie        = lie_var,
-                tcl_meta       = meta_var,
-                tcl_untch      = initTyVarUnique
+                tcl_errs            = errs_var,
+                tcl_loc             = mkGeneralSrcSpan (fsLit "Top level"),
+                tcl_ctxt            = [],
+                tcl_rdr             = emptyLocalRdrEnv,
+                tcl_th_ctxt         = topStage,
+                tcl_arrow_ctxt      = NoArrowCtxt,
+                tcl_env             = emptyNameEnv,
+                tcl_tidy            = emptyTidyEnv,
+                tcl_tyvars          = tvs_var,
+                tcl_lie             = lie_var,
+                tcl_meta            = meta_var,
+                tcl_untch           = initTyVarUnique,
+                tcl_named_wildcards = named_wc_map
              } ;
         } ;
 

compiler/typecheck/TcRnTypes.lhs
@@ -81,6 +81,10 @@ module TcRnTypes(
 	pprEvVars, pprEvVarWithType, 
         pprArising, pprArisingAt,
 
+        -- Named wildcards
+        NamedWildcardMap, emptyNamedWildcardMap, lookupNamedWildcard,
+        insertNamedWildcard,
+
 	-- Misc other types
 	TcId, TcIdSet, TcTyVarBind(..), TcTyVarBinds
 	
@@ -120,10 +124,13 @@ import DynFlags
 import Outputable
 import ListSetOps
 import FastString
+
 import Util
 
+import Data.Map (Map)
+import qualified Data.Map as Map
+import Data.Ord (comparing)
 import Data.Set (Set)
-
 \end{code}
 
 
@@ -446,10 +453,27 @@ data TcLclEnv		-- Changes as we move inside an expression
 	-- TcMetaTyVars have 
 	tcl_meta  :: TcRef Unique,  -- The next free unique for TcMetaTyVars
 		     		    -- Guaranteed to be allocated linearly
-	tcl_untch :: Unique	    -- Any TcMetaTyVar with 
+	tcl_untch :: Unique,	    -- Any TcMetaTyVar with 
 		     		    --     unique >= tcl_untch is touchable
 		     		    --     unique <  tcl_untch is untouchable
+        tcl_named_wildcards :: TcRef NamedWildcardMap -- Maps named wildcards to types
+                            -- Used to desugar named wildcards to meta variables.
+                            -- All named wildcards with the same name within one signature
+                            -- (TODOT scoped?) will be replaced by occurrences of the
+                            -- same metavariable.
     }
+
+-- TODOT better location
+type NamedWildcardMap = Map Name TcType
+
+emptyNamedWildcardMap :: NamedWildcardMap
+emptyNamedWildcardMap = Map.empty
+
+lookupNamedWildcard :: Name -> NamedWildcardMap -> Maybe TcType
+lookupNamedWildcard = Map.lookup
+
+insertNamedWildcard :: Name -> TcType -> NamedWildcardMap -> NamedWildcardMap
+insertNamedWildcard = Map.insert
 
 type TcTypeEnv = NameEnv TcTyThing
 

mrBliss/ghc-head

compiler/typecheck/TcRnMonad.lhs
@@ -96,6 +96,7 @@ initTc hsc_env hsc_src keep_rn_syntax mod do_this
                            Nothing             -> newIORef emptyNameEnv } ;
 
         dependent_files_var <- newIORef [] ;
+        named_wc_map        <- newIORef emptyNamedWildcardMap ;
 #ifdef GHCI
         th_topdecls_var      <- newIORef [] ;
         th_topnames_var      <- newIORef emptyNameSet ;
@@ -160,19 +161,20 @@ initTc hsc_env hsc_src keep_rn_syntax mod do_this
                 tcg_dependent_files = dependent_files_var
              } ;
              lcl_env = TcLclEnv {
-                tcl_errs       = errs_var,
-                tcl_loc        = mkGeneralSrcSpan (fsLit "Top level"),
-                tcl_ctxt       = [],
-                tcl_rdr        = emptyLocalRdrEnv,
-                tcl_th_ctxt    = topStage,
-                tcl_th_bndrs   = emptyNameEnv,
-                tcl_arrow_ctxt = NoArrowCtxt,
-                tcl_env        = emptyNameEnv,
-                tcl_bndrs      = [],
-                tcl_tidy       = emptyTidyEnv,
-                tcl_tyvars     = tvs_var,
-                tcl_lie        = lie_var,
-                tcl_untch      = noUntouchables
+                tcl_errs            = errs_var,
+                tcl_loc             = mkGeneralSrcSpan (fsLit "Top level"),
+                tcl_ctxt            = [],
+                tcl_rdr             = emptyLocalRdrEnv,
+                tcl_th_ctxt         = topStage,
+                tcl_th_bndrs        = emptyNameEnv,
+                tcl_arrow_ctxt      = NoArrowCtxt,
+                tcl_env             = emptyNameEnv,
+                tcl_bndrs           = [],
+                tcl_tidy            = emptyTidyEnv,
+                tcl_tyvars          = tvs_var,
+                tcl_lie             = lie_var,
+                tcl_untch           = noUntouchables,
+                tcl_named_wildcards = named_wc_map
              } ;
         } ;
 

compiler/typecheck/TcRnTypes.lhs
@@ -77,6 +77,10 @@ module TcRnTypes(
         pprEvVars, pprEvVarWithType,
         pprArising, pprArisingAt,
 
+        -- Named wildcards
+        NamedWildcardMap, emptyNamedWildcardMap, lookupNamedWildcard,
+        insertNamedWildcard,
+
         -- Misc other types
         TcId, TcIdSet, TcTyVarBind(..), TcTyVarBinds
 
@@ -117,11 +121,14 @@ import DynFlags
 import Outputable
 import ListSetOps
 import FastString
+import Util
 
-import Data.Set (Set)
+import qualified Data.Map as Map
+import Data.Map      ( Map )
+import Data.Ord      ( comparing )
+import Data.Set      ( Set )
 
 #ifdef GHCI
-import Data.Map      ( Map )
 import Data.Dynamic  ( Dynamic )
 import Data.Typeable ( TypeRep )
 
@@ -493,8 +500,25 @@ data TcLclEnv           -- Changes as we move inside an expression
                         -- Why mutable? see notes with tcGetGlobalTyVars
 
         tcl_lie  :: TcRef WantedConstraints,    -- Place to accumulate type constraints
-        tcl_errs :: TcRef Messages              -- Place to accumulate errors
+        tcl_errs :: TcRef Messages,             -- Place to accumulate errors
+        tcl_named_wildcards :: TcRef NamedWildcardMap -- Maps named wildcards to types
+                            -- Used to desugar named wildcards to meta variables.
+                            -- All named wildcards with the same name within one signature
+                            -- (TODOT scoped?) will be replaced by occurrences of the
+                            -- same metavariable.
     }
+
+-- TODOT better location
+type NamedWildcardMap = Map Name TcType
+
+emptyNamedWildcardMap :: NamedWildcardMap
+emptyNamedWildcardMap = Map.empty
+
+lookupNamedWildcard :: Name -> NamedWildcardMap -> Maybe TcType
+lookupNamedWildcard = Map.lookup
+
+insertNamedWildcard :: Name -> TcType -> NamedWildcardMap -> NamedWildcardMap
+insertNamedWildcard = Map.insert
 
 type TcTypeEnv = NameEnv TcTyThing
 